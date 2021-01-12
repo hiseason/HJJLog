6cc225c3eeaf3add5ab4da052ad54dbfd42d9be6
haojj <doublejia_hao@163.com>
2021-01-12 14:57:56 +0800

    create project

---
+++ HJJLog/HJJLog/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  HJJLog
+//
+//  Created by 郝旭姗 on 2021/1/12.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
